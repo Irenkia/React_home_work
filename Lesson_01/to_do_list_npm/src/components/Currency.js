d3a74c2fe8c84110373b4d444292178bfb8645a6
Iryna Tiutiun <tiutiuniryna@gmail.com>
2022-12-09 12:24:26 +0100

    Lesson 14 Classwork fixed a mistake in the name of the currency

--- Lesson_01/to_do_list_npm/src/components/Currency.js
+++ Lesson_01/to_do_list_npm/src/components/Currency.js
@@ -3,7 +3,7 @@ import React from "react";
 function Currency({ currency }) {
   return (
     <tr>
-      <td>{currency.name}</td>
+      <td>{currency.txt}</td>
       <td>{currency.cc}</td>
       <td>{currency.rate}</td>
     </tr>
