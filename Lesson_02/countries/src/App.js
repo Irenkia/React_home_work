d3ccf18e35905c6b6b946ec1ac646d559a9a6855
Iryna Tiutiun <tiutiuniryna@gmail.com>
2022-12-27 12:26:31 +0100

    task 02 create react app Countries

---
+++ Lesson_02/countries/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/Countries";
+
+function App() {
+  return (
+    <div className="container">
+      <h3 className="mb-3 p-3">Countries</h3>
+      <Countries />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson_02/countries/src/components/Countries.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+import { Table } from "react-bootstrap";
+
+function Countries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCountries(
+          data.map((country) => ({
+            alpha3Code: country.alpha3Code,
+            name: country.name,
+            capital: country.capital,
+            population: country.population,
+            area: country.area,
+            region: country.region,
+          }))
+        );
+      });
+  }, []);
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Capital</th>
+            <th>Population</th>
+            <th>Area</th>
+            <th>Region</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map((country) => (
+            <tr key={country.alpha3Code}>
+              <td>{country.name}</td>
+              <td>{country.capital}</td>
+              <td>{country.population}</td>
+              <td>{country.area}</td>
+              <td>{country.region}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+export default Countries;
